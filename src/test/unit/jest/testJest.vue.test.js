b0ba0269ceeb49089b3aaa582c3c2c89bd95cc70
thiagodantas90 <43214554+thiagodantas90@users.noreply.github.com>
2020-09-13 21:50:47 -0300

    Jest configurado
    Arquivo usado para testar o jest

---
+++ src/test/unit/jest/testJest.vue.test.js
@@ -0,0 +1,7 @@
+//Não precisa colocar o 'import {mount} from "@vue/test-utils"
+
+describe ("Iniciando teste unitário com jest", () => {
+    it('2 + 2 = 4', () =>{
+        expect(2 + 2).toBe(4)
+    })
+})
---
+++ src/test/unit/jest/userCad.vue.test.js
@@ -0,0 +1,8 @@
+// como importa o arquivo
+//import UserAdmin from 'D:\Users\Thiago Dantas\Projetos\lab-leite-frontend\src\components\admin\UserAdmin.vue'
+
+describe("Realizando teste unitário",()=>{
+    it('save usr', ()=>{
+
+    })
+})
